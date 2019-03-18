9bc75d3002af0364378713b5d9dbe8a6b1c9f3d5
ronakamilia27rpl <46396999+ronakamilia27rpl@users.noreply.github.com>
2019-03-18 10:55:46 +0700

    selesai dikerjakan

---
+++ form-login.php
@@ -0,0 +1,15 @@
+
+<html>
+<H1>Login</H1>
+<body>
+<form action="login.php" method="POST" >
+
+<label>Username :</label>
+<input type="text" name="username" /> 
+<br>
+<br>
+<label>Password :</label>
+<input type="password" name="password" />
+<br>
+<br>
+<input type="submit"  value="Login" />
